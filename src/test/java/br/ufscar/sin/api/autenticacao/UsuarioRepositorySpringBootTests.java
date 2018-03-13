68ec4b92b7e112e381721ad2fa7a60fef5014bd9
Bruno <blopes@ufscar.br>
2018-03-13 17:14:09 -0300

    Usar testEntityManager nos testes com anotação @SpringBootTest

--- src/test/java/br/ufscar/sin/api/autenticacao/UsuarioRepositorySpringBootTests.java
+++ src/test/java/br/ufscar/sin/api/autenticacao/UsuarioRepositorySpringBootTests.java
@@ -1,36 +1,31 @@
 package br.ufscar.sin.api.autenticacao;
 
 import br.ufscar.sin.api.ApiApplication;
-import br.ufscar.sin.api.config.database.ProducaoPostgresDatabaseConfiguration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import javax.persistence.EntityManager;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 @ExtendWith(SpringExtension.class)
-//@TestPropertySource(
-//        locations = {"classpath:test-application.properties","classpath:test-hibernate.properties"})
-@SpringBootTest (classes = {ApiApplication.class, ProducaoPostgresDatabaseConfiguration.class})
-//@ContextConfiguration(classes = {ProducaoPostgresDatabaseConfiguration.class})
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-//@AutoConfigureTestEntityManager
+@SpringBootTest (classes = {ApiApplication.class})
+@AutoConfigureTestEntityManager
 /**
  * Testes referentes ao UsuarioRepository
  */
+@Transactional
 public class UsuarioRepositorySpringBootTests {
 
 
     @Autowired
-    private EntityManager integracaoPostgresEntityManager;
+    private TestEntityManager testEntityManager;
 
     @Autowired
     private UsuarioRepository usuarioRepository;
@@ -42,9 +37,8 @@ public class UsuarioRepositorySpringBootTests {
     public void whenFindByUsername_thenReturnUser() {
         // given
         Usuario usuarioBruno = new Usuario("BrunoXXXX", "1234");
-//        integracaoPostgresEntityManager.persist(usuarioBruno);
-//        integracaoPostgresEntityManager.flush();
-        usuarioRepository.save(usuarioBruno);
+        testEntityManager.persist(usuarioBruno);
+        testEntityManager.flush();
 
         // when
         Usuario encontrado = usuarioRepository.findByUsername(usuarioBruno.getUsername());
